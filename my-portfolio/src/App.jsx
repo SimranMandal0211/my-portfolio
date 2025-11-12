82cba1439b6f62a8e01c6e3146d167a9f05a8ccb
Simran Mandal <simranmandal0211@gmail.com>
2025-11-12 13:42:45 +0530

    Basic structure to site

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,13 +1,25 @@
+import Navbar from './components/Navbar';
+import Home from './section/Home';
+import About from './section/About';
+import Skills from './section/Skills';
+import Projects from './section/Projects';
+import Experience from './section/Experience';
+import Testimonials from './section/Testimonials';
+import Contact from './section/Contact';
+import Footer from './section/Footer';
 
-
-function App() {
-
-
+export default function App(){
   return (
-    <>
-      
-    </>
+    <div className='relative gradient text-white'>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
   )
 }
-
-export default App
---
+++ my-portfolio/src/components/ParticalBackground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ParticalBackground = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ParticalBackground
